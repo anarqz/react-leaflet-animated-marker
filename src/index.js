8e809c17020cf7b086c3f58fe0bb8d11f96f636b
Alexandre Moraes <me@alcmora.es>
2018-05-26 16:56:33 -0300

    Working on code maintainability

--- src/index.js
+++ src/index.js
@@ -17,38 +17,22 @@ import Utils from './utils';
 class AnimatedMarker extends Marker {
 
   componentDidUpdate(): any {
-    // Here we return false to prevent the original marker
-    // To re-render itself, since we just need to update
-    // its position
     return false;
   }
 
   componentWillReceiveProps( nextProps: Object ): any {
     try {
-      // Animate the maker only if there's a considerable
-      // distance between previous and current position
       if (
-        Utils.isCoordinatesDistantEnough(
+        Utils.areCoordinatesDistantEnough(
           nextProps.position,
           this.props.position
         )
       ){
-        let position;
-        let movingInterval;
-        let curLat = this.props.position.lat;
-        let curLng = this.props.position.lng;
-        let newLat = nextProps.position.lat;
-        let newLng = nextProps.position.lng;
-        let threshold = 0.01;
-        movingInterval = setInterval( () => {
-          if ( threshold >= 1 ) clearInterval( movingInterval );
-          position = new L.LatLng(
-            ( curLat + ( newLat - curLat ) * threshold ),
-            ( curLng + ( newLng - curLng ) * threshold )
-          );
-          this.leafletElement.setLatLng( position );
-          threshold += 0.1;
-        }, 400 );
+        Utils.animateMarker(
+          this.leafletElement,
+          this.props.position,
+          nextProps.position
+        );
         return false;
       }
     }
--- src/utils.js
+++ src/utils.js
@@ -21,9 +21,35 @@ class Utils {
    * @param {L.LatLng} coord_two The coordinate to compare
    * @return {boolean}
    */
-  isCoordinatesDistantEnough( coord_one: L.LatLng, coord_two: L.LatLng ): boolean {
+  areCoordinatesDistantEnough( coord_one: L.LatLng, coord_two: L.LatLng ): boolean {
     return JSON.stringify( coord_one.lat.toString().substring( 8, 3 ) ) !==
       JSON.stringify( coord_two.lat.toString().substring( 8, 3 ) );
+  }
+
+  /**
+   * Animate a leaflet element marker between two coordinates
+   *
+   * @param {L} leafletElement The leaflet element from marker
+   * @param {L.LatLng} coordFrom The coordinate to go from
+   * @param {L.LatLng} coordTo The coordinate to go to
+   */
+  animateMarker( leafletElement: any, coordFrom: L.LatLng, coordTo: L.LatLng ): void {
+    let position;
+    let movingInterval;
+    let curLat = coordFrom.lat;
+    let curLng = coordFrom.lng;
+    let newLat = coordTo.lat;
+    let newLng = coordTo.lng;
+    let threshold = 0.01;
+    movingInterval = setInterval( () => {
+      if ( threshold >= 1 ) clearInterval( movingInterval );
+      position = new L.LatLng(
+        ( curLat + ( newLat - curLat ) * threshold ),
+        ( curLng + ( newLng - curLng ) * threshold )
+      );
+      leafletElement.setLatLng( position );
+      threshold += 0.1;
+    }, 400 );
   }
 
 }
